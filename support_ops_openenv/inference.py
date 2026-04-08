2031184e22d0cddc3d570b93687f91ce5ef16d92
Chethan Gowda S.N <iamchethan2813@gmail.com>
2026-04-08 23:29:18 +0530

    Update inference.py

--- support_ops_openenv/inference.py
+++ support_ops_openenv/inference.py
@@ -10,7 +10,7 @@ from openai import OpenAI
 
 API_BASE_URL = os.getenv("API_BASE_URL", "https://router.huggingface.co/v1")
 MODEL_NAME = os.getenv("MODEL_NAME", "meta-llama/Llama-3.1-8B-Instruct")
-HF_TOKEN = os.getenv("HF_TOKEN")
+HF_TOKEN = os.getenv("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")
 ENV_BASE_URL = os.getenv("ENV_BASE_URL", "http://127.0.0.1:8000")
 
 
